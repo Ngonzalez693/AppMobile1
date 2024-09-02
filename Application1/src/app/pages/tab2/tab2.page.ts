a6def27532d214df7f9cd771302b1718d1f0dc94
Ngonzalez693 <nicogonto@gmail.com>
2024-09-02 10:06:43 -0500

    lugares

--- Application1/src/app/pages/tab2/tab2.page.ts
+++ Application1/src/app/pages/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RickyMortyBdService } from 'src/app/services/ricky-morty-bd.service';
 
 @Component({
   selector: 'app-tab2',
@@ -6,21 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page implements OnInit {
-  items: string[] = [];
+  lugares: any[] = [];
 
-  ngOnInit() {
-    this.generateItems();
+  constructor(private bd: RickyMortyBdService) { }
+
+  async cargarLugares() {
+    //this.cargando = true;
+    await this.bd
+      .getAllLugares()
+      .toPromise()
+      .then((resp: any) => {
+
+        this.lugares = resp.results;
+
+        console.log('MISLUGARES', this.lugares);
+
+      });
   }
 
-  private generateItems() {
-    const count = this.items.length + 1;
-    for (let i = 0; i < 50; i++) {
-      this.items.push(`Lugar ${count + i}`);
-    }
+  ngOnInit() {
+    this.cargarLugares();
   }
   
   onIonInfinite(ev: Event) {
-    this.generateItems();
+    this.cargarLugares();
     setTimeout(() => {
       const infiniteScroll = ev.target as HTMLIonInfiniteScrollElement;
       infiniteScroll.complete();
--- Application1/src/app/services/ricky-morty-bd.service.ts
+++ Application1/src/app/services/ricky-morty-bd.service.ts
@@ -10,13 +10,14 @@ export class RickyMortyBdService {
 
   constructor(private http: HttpClient) { }
 
+  
   getAllPersonajes(): any {
 
     let url = `${URL_RM}/character`;
 
     return this.http.get(url, {}).pipe(
       map((res: any) => {
-        console.log('PERSONAJES_RK', res);
+        console.log('PERSONAJES_RM', res);
         return res;
       })
     );
@@ -29,6 +30,30 @@ export class RickyMortyBdService {
     return this.http.get(url, {}).pipe(
       map((res: any) => {
         console.log('PERSONAJE', res);
+        return res;
+      })
+    );
+  }
+
+  getAllLugares(): any {
+
+    let url = `${URL_RM}/location`;
+
+    return this.http.get(url, {}).pipe(
+      map((res: any) => {
+        console.log('Lugares_RM', res);
+        return res;
+      })
+    );
+  }
+
+  getLugaresById(unId:number): any {
+
+    let url = `${URL_RM}/location/${unId}`;
+
+    return this.http.get(url, {}).pipe(
+      map((res: any) => {
+        console.log('LUGAR', res);
         return res;
       })
     );
